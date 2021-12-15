3ee191b639d54cf6059521745d824d0411f0d1a1
Daniil <orgfrank@icloud.com>
2021-12-15 13:55:27 +0600

    Initial Commit

---
+++ EmojiContext/Model/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiContext
+//
+//  Created by Даниил Франк on 15.12.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
